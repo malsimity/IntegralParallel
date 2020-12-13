b77247e6616c4650702da00df9a96779335e5ff8
muravlevadiana <73022838+muravlevadiana@users.noreply.github.com>
2020-12-13 22:21:25 +0300

    Добавила тесты

---
+++ Integral.Tests/Integral.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Integral.Classes;
+
+namespace Integral.Tests
+{
+    [TestClass]
+    public class Integral
+    {
+        [TestMethod]
+        public void S_xx_0_10()
+        {
+            //arrange
+            double expected = 333.333;
+            double a = 0;
+            double b = 10;
+            int n = 10000;
+            ICalculator calcul = new Simpson();
+
+            //act
+            double actual = calcul.CalculateParallel(a, b, n, x => x * x);
+
+            //assert
+            Assert.AreEqual(expected, actual, 0.001);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void S_xx_Negative_Step()
+        {
+            //arrange
+            double expected = 333.333;
+            double a = 0;
+            double b = 10;
+            int n = -10000;
+            ICalculator calcul = new Simpson();
+
+            //act
+            double actual = calcul.CalculateParallel(a, b, n, x => x * x);
+
+            //assert
+            Assert.AreEqual(expected, actual, 0.001);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void S_xx_Odd_Step()
+        {
+            //arrange
+            double expected = 333.333;
+            double a = 0;
+            double b = 10;
+            int n = 1001;
+            ICalculator calcul = new Simpson();
+
+            //act
+            double actual = calcul.CalculateParallel(a, b, n, x => x * x);
+
+            //assert
+            Assert.AreEqual(expected, actual, 0.001);
+        }
+        [TestMethod]
+        public void S_xx_10_0()
+        {
+            //arrange
+            double expected = -333.333;
+            double a = 10;
+            double b = 0;
+            int n = 10000;
+            ICalculator calcul = new Simpson();
+
+            //act
+            double actual = calcul.Calculate(a, b, n, x => x * x);
+
+            //assert
+            Assert.AreEqual(expected, actual, 0.001);
+        }
+        [TestMethod]
+        public void S_Step_0()
+        {
+            //arrange
+            double expected = 0;
+            double a = 10;
+            double b = 10;
+            int n = 10000;
+            ICalculator calcul = new Simpson();
+
+            //act
+            double actual = calcul.CalculateParallel(a, b, n, x => x * x);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Integral.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Integral.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Integral.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4f717844-8b71-4d9c-985c-19559ae17b08")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
